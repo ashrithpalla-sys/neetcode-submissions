ea867bd988d4d3736b824042b1fd423426f5c1e6
Ashrith Palla <ashrithpalla@gmail.com>
2026-08-07 03:32:24 -0400

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 2)
+
+        for x in range(2, n + 1):
+            dp[x] = min(dp[x - 2] + cost[x - 2], dp[x - 1] + cost[x - 1])
+
+        return dp[n]
+        
